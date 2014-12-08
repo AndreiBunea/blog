b93677f90907cc263ca6710c97eaf53c38d98ea8
bunea <bunea.andrei92@gmail.com>
2014-12-08 18:35:43 +0200

    set root to posts index and update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208105821) do
+ActiveRecord::Schema.define(version: 20141208163225) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20141208105821) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
