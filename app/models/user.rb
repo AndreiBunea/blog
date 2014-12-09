6a69014cd0d7f8fdc3c156d283cb3e1532675b9a
bunea <bunea.andrei92@gmail.com>
2014-12-09 22:18:00 +0200

    add authenticate method on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :password, presence: true, length: { minimum: 6 }
+
+  def self.authorize(name, password)
+    user = User.find_by_name name
+
+    if user && user.password == password
+      return user
+    else
+      return false
+    end
+  end
 end
